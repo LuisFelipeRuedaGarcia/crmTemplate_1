ad41f93207057b605349463391ac0b07e1468940
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-05-25 09:46:17 -0500

    feat (first commit): primero

---
+++ crmTemplate/Estudiantes/config.php
@@ -0,0 +1,112 @@
+<?php
+require_once("db.php");
+
+class Config{
+
+    private $id;
+    private $nombres;
+    private $direccion;
+    private $logros;
+    protected $dbCnx;
+
+    public function __construct($id = 0, $nombres ="", $direccion = "", $logros = ""){
+
+        $this->id = $id;
+        $this->nombres = $nombres;
+        $this->direccion = $direccion;
+        $this->logros = $logros;
+        $this->dbCnx = new PDO(DB_TYPE.":host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PWD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setNombres($nombres){
+        $this->nombres = $nombres;
+    }
+
+    public function getNombres(){
+        return $this->nombres;
+    }
+
+    public function setDireccion($direccion){
+        $this->direccion = $direccion;
+    }
+
+    public function getDireccion(){
+        return $this->direccion;
+    }
+
+    public function setLogros($logros){
+        $this->logros = $logros;
+    }
+
+    public function getLogros(){
+        return $this->logros;
+    }
+
+    public function insertData(){
+        try {
+             $stm = $this-> dbCnx -> prepare("INSERT INTO campers (nombres, direccion,logros) values(?,?,?)");
+            $stm -> execute([$this->nombres, $this->direccion, $this->logros]);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function selectAll(){
+        try {
+            $stm = $this->dbCnx->prepare("SELECT * FROM campers");
+            $stm-> execute();
+            return $stm-> fetchAll();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function delete(){
+        try {
+            $stm = $this->dbCnx->prepare("DELETE FROM campers WHERE id = ?");
+            $stm -> execute ([$this->id]);
+            return $stm->fetchAll();
+            echo "<script>alert('Borrado Exitosamente');document.location='estudiantes.php'</script>";
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function selectOne(){
+
+        try {
+            $stm = $this->dbCnx->prepare("SELECT * FROM campers WHERE id = ?");
+            $stm -> execute([$this->id]);
+            return $stm->fetchAll();
+            echo "<script>alert('Borrado Exitosamente');document.location='estudiantes.php'</script>";
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    
+    public function update(){
+    try {
+        echo "hasta aquí bien";
+        $stm = $this-> dbCnx -> prepare("UPDATE campers SET NOMBRES = ?, direccion = ?, logros = ? WHERE id = ?");
+        echo "<br>depués del prepare<br>";
+        $stm -> execute([$this->NOMBRES, $this->direccion, $this->logros, $this->id]);
+        echo "<br>depués del execute<br>";
+
+    } catch (Exception $e) {
+        echo "hubo un error";
+        return $e->getMessage();
+    }
+    }
+}
+
+
+?>
---
+++ crmTemplate/Estudiantes/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE campus;
+
+CREATE TABLE campers(
+    id INT primary key AUTO_INCREMENT,
+    NOMBRES VARCHAR (50) NOT NULL,
+    direccion VARCHAR (50),
+    logros VARCHAR (60)
+);
+
